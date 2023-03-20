1d3a05b418dbee53f03120423ca9394bae43dd13
Uğur <ugursaritepe@windowslive.com>
2023-03-20 17:06:57 +0100

    Update streamlit_app.py
    indention bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())    
     streamlit.dataframe(fruityvice_normalized)
- except URLError as e:
+except URLError as e:
   streamlit.error()
 
 
